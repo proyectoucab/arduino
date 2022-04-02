3648a5ce4f9880aefe68e2f025786fb84edb4ede
proyectoucab <75284217+proyectoucab@users.noreply.github.com>
2022-04-02 14:34:57 -0400

    CAMBIOS motor instrucciones

--- MotorL298.ino
+++ MotorL298.ino
@@ -1,10 +1,19 @@
+#include<AFMotor.h>
+
+AF_DCMotor motor(1);
+
 const int ENA = 8;
 const int IN1 = 9;
 const int IN2 = 10;
 
+char entrada = 'F';
 
 
 void setup() {
+
+ 
+  Serial.begin(9600);
+  
   pinMode(ENA, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -14,20 +23,35 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(ENA, HIGH);
-  delay(5000);
-  // MOtor no gira
-  digitalWrite(ENA, LOW);
-  delay(1000);
-  // motor gira en sentido inverso
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,HIGH);
-  
-  digitalWrite(ENA, HIGH);
-  delay(5000);
-  
-  digitalWrite(ENA,LOW);
-  delay(1000);
+
+  if(Serial.available() >0){
+    // lo que reciba del teclado, se guardarà en la variable comando
+    entrada = Serial.read();
+
+    switch(entrada){
+      case 'F':
+            digitalWrite(IN1, HIGH);
+            digitalWrite(IN2, LOW);
+            digitalWrite(ENA, HIGH);
+        break;
+
+        case 'S':
+          // MOtor no gira
+            digitalWrite(ENA,LOW);
+            
+         break;
+
+         case 'B':
+          digitalWrite(IN1,LOW);
+          digitalWrite(IN2,HIGH);
+          digitalWrite(ENA, HIGH);        
+         
+         break;
+      
+      }
+    delay(500);
+    }
   }
+
+  // motor gira en sentido inverso
+  
